1451282fa06a305d85f0d7840b4231880e674ff3
Alan Duncan <duncan.alan@mac.com>
2016-03-25 21:32:45 -0400

    replace before post render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-hexo.extend.filter.register('after_render', function(data) {
+hexo.extend.filter.register('before_post_render', function(data) {
     var re = /(\|{2}?)((.|\n)+?)(\|{2}?)/gm;
     data.content = data.content.replace(re,'<span class="rsb">$2</span>');
 });
