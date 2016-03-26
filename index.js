22ef38d70412bbbf9cb53de7bb8bee6604a98304
Alan Duncan <duncan.alan@mac.com>
2016-03-25 21:18:25 -0400

    content belongs to data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 hexo.extend.filter.register('after_post_render', function(data) {
     var re = /(\|{2}?)((.|\n)+?)(\|{2}?)/gm;
-    data.content = content.replace(re,'<span class="rsb">$2</span>');
+    data.content = data.content.replace(re,'<span class="rsb">$2</span>');
 });
