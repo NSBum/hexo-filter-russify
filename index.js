366dff4c0062e812e2b6eee616d28a4f695fb2d9
Alan Duncan <duncan.alan@mac.com>
2016-03-26 05:14:56 -0400

    change to after_render

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-hexo.extend.filter.register('after_post_render', function(data) {
+hexo.extend.filter.register('after_render:html', function(data) {
     var re = /(\|{2}?)((.|\n)+?)(\|{2}?)/gm;
-    data.content = data.content.replace(re,'<span class="rsb">$2</span>');
-    return data;
+    var result = str.replace(re,'<span class="rsb">$2</span>');
+    return result;
+    //data.content = data.content.replace(re,'<span class="rsb">$2</span>');
+    //return data;
 });
