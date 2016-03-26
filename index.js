674fdf3442dfe5b80d5e5376e7d7ed9c5226bc5d
Alan Duncan <duncan.alan@mac.com>
2016-03-26 05:20:55 -0400

    apply filter after render

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-hexo.extend.filter.register('after_render:html', function(data) {
+hexo.extend.filter.register('after_render:html', function(str,data) {
     var re = /(\|{2}?)((.|\n)+?)(\|{2}?)/gm;
     var result = str.replace(re,'<span class="rsb">$2</span>');
     return result;
-    //data.content = data.content.replace(re,'<span class="rsb">$2</span>');
-    //return data;
 });
