fb0e842d5c016b4fadb8c4f2ac38164c58fd6250
Alan Duncan <duncan.alan@mac.com>
2016-03-25 21:46:11 -0400

    after_post_render

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-hexo.extend.filter.register('before_post_render', function(data) {
+hexo.extend.filter.register('after_post_render', function(data) {
     var re = /(\|{2}?)((.|\n)+?)(\|{2}?)/gm;
     data.content = data.content.replace(re,'<span class="rsb">$2</span>');
+    return data;
 });
